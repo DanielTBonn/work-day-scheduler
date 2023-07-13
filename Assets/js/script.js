3ccd567871c898ced653eea6bb49d4fe0a562684
Daniel Bonn <danieltbonn@gmail.com>
2023-07-13 00:17:37 -0500

    deleted comments in js and properly spaced code

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -13,7 +13,6 @@ $(function () {
   var day = today.format("D");
   var month = today.format("MM");
   var year = today.format("YYYY");
-
   var date = JSON.parse(localStorage.getItem("date"));
 
   function setDate() {
@@ -23,7 +22,6 @@ $(function () {
     date = JSON.parse(localStorage.getItem("date"));
   }
 
-  
   if (!localStorage.getItem("date")) {
     setDate();
   } 
@@ -40,20 +38,12 @@ $(function () {
 
   checkDate();
 
-  console.log("dates: ", date)
-
   var saveBtn = $(".saveBtn");
   saveBtn.on("click", function() {
     var parentId = $(this).parent().attr("id");
     var userInput = $(this).prev().val();
     localStorage.setItem(parentId, userInput);
   });
-  
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
 
   var currentHour = today.format("H");
   var formattedDate = today.format("dddd[,] MMMM DD")
@@ -73,12 +63,6 @@ $(function () {
     }
   }
 
-  currentTime();
-
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-
   function populateCalendar() {
     for (let i = 0; i < hourId.length; i++) {
       var hour = hourId[i].getAttribute("id");
@@ -86,15 +70,13 @@ $(function () {
       $("#" + hour).children("textarea").text(hourMessage)
     }
   }
-
+  
+  currentTime();
   populateCalendar();
-  // TODO: Add code to display the current date in the header of the page.
-
   
   function addDate() {
     $("#currentDay").text(formattedDate);
   }
   addDate();
 });
-console.log(localStorage);
 
